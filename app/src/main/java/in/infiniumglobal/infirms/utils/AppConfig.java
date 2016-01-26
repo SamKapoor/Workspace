5f7f2b3d9a5b92138e99bc5df187ced54e47c0a9
DevAppStartup <hiralmpatel19@outlook.com>
2016-01-26 15:44:33 +0530

    appconfig data add

--- app/src/main/java/in/infiniumglobal/infirms/utils/AppConfig.java
+++ app/src/main/java/in/infiniumglobal/infirms/utils/AppConfig.java
@@ -8,4 +8,10 @@ import android.database.Cursor;
 public class AppConfig {
 
     public static Cursor customerData = null;
+    public static int areaID = 0;
+    public static int revenueID = 0;
+    public static int locationID = 0;
+    public static String locationItem = "";
+    public static String revenueItem = "";
+    public static String areaItem = "";
 }
