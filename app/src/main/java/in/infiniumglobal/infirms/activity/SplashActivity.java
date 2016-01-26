6cd90081d99329cc97b6041af9cc5df005c1f212
DevAppStartup <hiralmpatel19@outlook.com>
2016-01-26 14:23:50 +0530

    Added splash screen

---
+++ app/src/main/java/in/infiniumglobal/infirms/activity/SplashActivity.java
@@ -0,0 +1,36 @@
+package in.infiniumglobal.infirms.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import in.infiniumglobal.infirms.R;
+import in.infiniumglobal.infirms.utils.Common;
+
+/**
+ * Created by Hiral on 1/26/2016.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent mIntent;
+                if (Common.getStringPrefrences(SplashActivity.this, getString(R.string.pref)).trim().equals("")) {
+                    mIntent = new Intent(SplashActivity.this, LoginActivity.class);
+                } else {
+                    mIntent = new Intent(SplashActivity.this, RevenueSelectionActivity.class);
+                }
+                startActivity(mIntent);
+                finish();
+            }
+        }, 3000);
+    }
+}
